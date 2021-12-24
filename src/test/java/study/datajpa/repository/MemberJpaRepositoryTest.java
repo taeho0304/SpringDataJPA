56d1775c92b16f93a5283cfe6acf473496cd043a
taeho0304 <tdj03063@gmail.com>
2021-12-24 18:44:05 +0900

    Feat : 순수 JPA 기반 Repository 만들기

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -9,11 +9,13 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional // -> Test가 끝날 때 다 Rollback을 시켜주고 JPA의 영속성 Context도 flush 안함 ( DB에 아무 쿼리도 보내지 않음 )
-@Rollback(false) // 등록 쿼리 확인용 -> Rollback을 안하고 Commit 시킴 ( 실무에서는 사용 x )
+@Rollback(false) // 등록 쿼리 확인용 -> Rollback을 안하고 Commit 시킴 ( 실무에서는 사용 x ) -> 변경감지 ( Duty Checking )
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
@@ -30,7 +32,38 @@ class MemberJpaRepositoryTest {
         /**
          * true가 나옴
          * JPA 특성상 같은 Transaction에서는 영속성 context의 동일성을 보장해줌 ( 1차 캐시 )
+         * 생성 객체( member )와 가져온 객체( findMember )가 동일함
          * */
         Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
     }
 }
